682b994573378be09c1cd8c73658f66ed66e79b4
Shagun Malik <shagunmalik41@gmail.com>
2026-05-18 14:03:23 +0530

    Completed working RAG Study Assistant

--- ai-career-platform/backend/app/routers/study.py
+++ ai-career-platform/backend/app/routers/study.py
@@ -4,13 +4,25 @@ from pydantic import BaseModel
 from pypdf import PdfReader
 import io
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from sentence_transformers import SentenceTransformer
 
 import chromadb
 
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
 router = APIRouter()
+
+# Load environment variables
+load_dotenv()
+
+# Groq client
+groq_client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
 
 # ChromaDB setup
 client = chromadb.Client()
@@ -99,10 +111,16 @@ class QuestionRequest(BaseModel):
 @router.post("/ask-question")
 async def ask_question(data: QuestionRequest):
 
-    # Generate embedding for user question
+    # Check if collection has data
+    if collection.count() == 0:
+        return {
+            "message": "Please upload a PDF first."
+        }
+
+    # Generate embedding for question
     query_embedding = model.encode([data.question]).tolist()
 
-    # Search relevant chunks
+    # Retrieve relevant chunks
     results = collection.query(
         query_embeddings=query_embedding,
         n_results=3
@@ -110,9 +128,44 @@ async def ask_question(data: QuestionRequest):
 
     documents = results["documents"][0]
 
-    print("RETRIEVED DOCUMENTS:", documents)
+    # If no relevant documents found
+    if len(documents) == 0:
+        return {
+            "message": "No relevant content found."
+        }
+
+    context = "\n".join(documents)
+
+    # Prompt for AI
+    prompt = f"""
+You are an AI Study Assistant.
+
+Answer the question ONLY from the provided context.
+
+Context:
+{context}
+
+Question:
+{data.question}
+"""
+
+    print("QUESTION:", data.question)
+
+    # Groq API call
+    chat_completion = groq_client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        model="llama-3.1-8b-instant"
+    )
+
+    answer = chat_completion.choices[0].message.content
 
     return {
         "question": data.question,
+        "answer": answer,
         "retrieved_chunks": documents
     }
